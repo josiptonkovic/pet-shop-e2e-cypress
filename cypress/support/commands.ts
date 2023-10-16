b2d057625cfc4b0c092e18a2c804fb54ed163853
Josip Tonkovic <josip.tonkovic@outlook.com>
2023-10-16 16:58:43 +0200

    Add custom login command
    Created a Cypress Custom command that uses the retrieved users json to verifay the login funcionality. the custom command 4 test cases to test the funcionality.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,10 +1,12 @@
 import cypress = require("cypress");
+import { homePage } from "../pages/Homepge";
 
 declare global {
     namespace Cypress {
         interface Chainable {
             retrieveToken(): Cypress.Chainable<{ authToken: any; data: any; }>
             retrieveUsers(): Cypress.Chainable<{ body: { data: any[] } }>
+            loginUser(loginCase: 'validUser' | 'invalidEmail' | 'invalidUser' | 'invalidPassword'): Cypress.Chainable<{}>
         }
     }
 };
@@ -41,5 +43,51 @@ Cypress.Commands.add('retrieveUsers', () => {
             const retrievedUsers = response.body.data;
             cy.writeFile('cypress/fixtures/users.json', JSON.stringify(retrievedUsers, null, 2));
         });
+    });
+});
+
+Cypress.Commands.add('loginUser', (loginCase: 'validUser' | 'invalidEmail' | 'invalidUser' | 'invalidPassword' = 'validUser') => {
+    return cy.retrieveUsers().then(() => {
+        let user: string;
+        let password: string;
+        const userList = 'users.json';
+        
+        const loginSteps = (user: string, password: string) => {
+            homePage.loginBtn1Element.click();
+            homePage.usernameElement.type(user);
+            homePage.passwordElement.type(password);
+            homePage.loginBtn2Element.click();
+        };
+
+        switch (loginCase) {
+
+            case 'invalidEmail':
+                user = 'wrong@user.com';
+                password = `${Cypress.env('USER_PASSWORD')}`;
+                loginSteps(user, password);
+            break;
+  
+            case 'invalidUser':
+                user = 'wrong@user.com';
+                password = 'wrongpassword';
+                loginSteps(user, password);
+            break;
+  
+            case 'invalidPassword':
+                return cy.fixture(userList).then((availableUsers: { email: string }[]) => {
+                    const randomIndex = Math.floor(Math.random() * availableUsers.length);
+                    user = availableUsers[randomIndex].email;
+                    password = 'wrongpassword';
+                    loginSteps(user, password);
+                });
+  
+            default:
+                return cy.fixture(userList).then((availableUsers: { email: string }[]) => {
+                    const randomIndex = Math.floor(Math.random() * availableUsers.length);
+                    user = availableUsers[randomIndex].email;
+                    password = `${Cypress.env('USER_PASSWORD')}`;
+                    loginSteps(user, password);
+                });
+        }
     });
 });
