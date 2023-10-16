4a450880471697abe3b467173c75a40fd76f1097
Josip Tonkovic <josip.tonkovic@outlook.com>
2023-10-16 15:47:14 +0200

    Add function to retrieve token
    Created a Cypress Custom command to fetch admin API token that can later be used in other commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,29 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+import cypress = require("cypress");
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            retrieveToken(): Cypress.Chainable<{ authToken: any; data: any; }>
+        }
+    }
+};
+
+Cypress.Commands.add('retrieveToken', () => {
+    return cy.request({
+        method: 'POST',
+        url: `${Cypress.env('STORE_URL')}api/v1/admin/login`,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+            email: Cypress.env('ADMIN_EMAIL'),
+            password: Cypress.env('ADMIN_PASSWORD')
+        }
+    }).then((response) => {
+        const authToken = response.body.data.token;
+        return {
+            authToken,
+            data: response.body,
+        };
+    });
+});
