cb840716219c8acef18d1fdef7483129c661f674
Josip Tonkovic <josip.tonkovic@outlook.com>
2023-10-16 15:36:50 +0200

    Add .env plugin, configuration and file
    Installed the cypress-plugin-dotenv plugin to fetch credentials and environment from an .env file. Configured the cofig file to use the dotenv plugin and created the .env file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
+import { dotenv } from 'cypress-plugin-dotenv';
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    ...process.env
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return dotenv(config);
     },
   },
 });
