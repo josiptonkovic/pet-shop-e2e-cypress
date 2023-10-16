3a4bfff12b8929f6834fca5a938392d05e930ede
Josip Tonkovic <josip.tonkovic@outlook.com>
2023-10-16 15:58:21 +0200

    Add command to retrieve Users
    Created a custom command that will fetch the user list from the API and store it in a json file inside the fixtures folder to be used later by other tests and functions

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,6 +4,7 @@ declare global {
     namespace Cypress {
         interface Chainable {
             retrieveToken(): Cypress.Chainable<{ authToken: any; data: any; }>
+            retrieveUsers(): Cypress.Chainable<{ body: { data: any[] } }>
         }
     }
 };
@@ -25,5 +26,20 @@ Cypress.Commands.add('retrieveToken', () => {
             authToken,
             data: response.body,
         };
+    });
+});
+
+Cypress.Commands.add('retrieveUsers', () => {
+    cy.retrieveToken().then(({ authToken }) => {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.env('STORE_URL')}api/v1/admin/user-listing`,
+            headers: {
+                Authorization: `Bearer ${authToken}`,
+            },
+        }).then((response) => {
+            const retrievedUsers = response.body.data;
+            cy.writeFile('cypress/fixtures/users.json', JSON.stringify(retrievedUsers, null, 2));
+        });
     });
 });
